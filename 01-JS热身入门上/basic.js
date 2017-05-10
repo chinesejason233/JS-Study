8be47ebab61378e0cf7e43968ea17a6b5ce6c84e
duyangsen <562834554@qq.com>
2017-05-10 15:07:53 +0800

    01-弹窗设置div属性

---
+++ 01-JS热身入门上/basic.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ASUS on 2017/5/10.
+ */
+/**
+ *
+ * @param id
+ * @returns {Element}
+ */
+function getId(id) {
+    var idName= document.getElementById(id);
+    return idName;
+}
